046e3501cc451b63bfa15c2eab87090731bd3f85
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-04-14 13:06:50 +0200

    Added a new theme.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {SongComponent} from '../_components/song/song.component';
 import {AlbumComponent} from '../_components/album/album.component';
 import {PlaylistComponent} from '../_components/playlist/playlist.component';
 import {UserComponent} from '../_components/user/user.component';
+import {AppComponent} from './app.component';
 
 const routes: Routes = [
   {path: 'songs', component: SongComponent},
   {path: 'albums', component: AlbumComponent},
   {path: 'playlists', component: PlaylistComponent},
-  {path: 'users', component: UserComponent}
+  {path: 'users', component: UserComponent},
+  {path: '#', component: AppComponent}
   ];
 
 @NgModule({
