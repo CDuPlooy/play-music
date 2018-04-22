f217e67162fffa6a0fbfd305f35e12d81d5681a3
RagingGrim <armandconnor.duplooy@gmail.com>
2018-04-22 16:46:27 +0200

    Finished settings page.

--- src/_components/settings/settings.component.ts
+++ src/_components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Artist} from '../../_models';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
+  public blockee: string;
+  public artists: string[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+  public block() {
+    console.log(this.blockee);
+    if (this.blockee.length !== 0) {
+        this.artists.push(this.blockee);
+    }
+    this.blockee = '';
+  }
 }
